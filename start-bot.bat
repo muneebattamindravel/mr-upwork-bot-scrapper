8a9c4761ce4870f629b215765dc2afb5cfe28f5f
Muneeb Atta Khan <30898560+muneebattamindravel@users.noreply.github.com>
2025-06-14 15:04:24 +0500

    quick update

--- start-bot.bat
+++ start-bot.bat
@@ -1,3 +1,3 @@
 @echo off
 cd /d "C:\Users\Administrator\Desktop\mr-upwork-bot-scrapper"
-start "" cmd /k "set BOT_AGENT_LAUNCHED=1 && npm start -- --bot-agent=ec2-micro-1"
+start "" cmd /k npm start
