e5b188608c67ee0277daa8fe24bb0c294df6bd3e
Muneeb Atta Khan <30898560+muneebattamindravel@users.noreply.github.com>
2025-06-15 15:21:23 +0500

    quick update

--- start-bot.bat
+++ start-bot.bat
@@ -1,3 +1,8 @@
+@REM @echo off
+@REM cd /d "C:\Users\Administrator\Desktop\mr-upwork-bot-scrapper"
+@REM start "UPWORK_SCRAPER_BOT_WINDOW" cmd /k "electron . --bot-tag=ec2-t2micro-scraper-bot"
+
 @echo off
 cd /d "C:\Users\Administrator\Desktop\mr-upwork-bot-scrapper"
-start "UPWORK_SCRAPER_BOT_WINDOW" cmd /k "electron . --bot-tag=ec2-t2micro-scraper-bot"
+start "UPWORK_SCRAPER_BOT_WINDOW" electron . --bot-tag=ec2-t2micro-scraper-bot
+exit
