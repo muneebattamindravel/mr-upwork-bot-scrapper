7155484bf8f62af3ee011748b8a8ed87ac291ea0
Muneeb Atta Khan <30898560+muneebattamindravel@users.noreply.github.com>
2025-06-17 16:32:03 +0500

    quick update

--- modules/utils.js
+++ modules/utils.js
@@ -34,6 +34,8 @@ async function shouldVisitJob(url) {
     const response = await axios.post(`http://${process.env.SERVER_URL}/api/jobs/shouldVisit`, {
       url: url.split('?')[0]
     });
+
+    log(`response of should visit`, response.data);
     return response.data?.shouldVisit === true;
   } catch (err) {
     console.error(`[shouldVisitJob] Error checking job existence: ${err.message}`);
