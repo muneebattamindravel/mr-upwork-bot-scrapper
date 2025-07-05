043cf04c420248deeafabde410bb61aac9d4777f
Muneeb Atta Khan <30898560+muneebattamindravel@users.noreply.github.com>
2025-07-05 19:25:26 +0500

    quick update

--- modules/botSettings.js
+++ modules/botSettings.js
@@ -13,8 +13,8 @@ async function getBotSettings(botId) {
       return cachedSettings;
     }
 
-    console.log(`calling ${process.env.BRAIN_BASE_URL}/bots/bot-settings/${botId}`)
-    const response = await axios.get(`${process.env.BRAIN_BASE_URL}/bots/bot-settings/${botId}`);
+    console.log(`calling ${process.env.BRAIN_BASE_URL}/bots/settings/${botId}`)
+    const response = await axios.get(`${process.env.BRAIN_BASE_URL}/bots/settings/${botId}`);
     console.log(`settings response: `, response);
 
     if (response.data?.success === true && response.data?.data) {
